7adec33c95dc052750b51cceb4ae4f25ffff6727
Gabor Szabo <szabgab@gmail.com>
2011-07-31 09:55:10 +0300

    remove comments from mailman.js add experimental yml file

--- html/js/mailman.js
+++ html/js/mailman.js
@@ -1,9 +1,6 @@
  $(document).ready(function() {
    $("form.subscribe").submit(function() {
-     //alert( $(this).attr('action') );
-     //alert( $(this).serialize() );
      var url = $(this).attr('action') + '?' + $(this).serialize();
-//alert(url);
      $.get(url, function(r) {
          window.location = "/thankyou.html";
      });
