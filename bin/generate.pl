22bec4d1a505d5987dea5a03db775a99b7a5aa8c
Gabor Szabo <gabor@szabgab.com>
2011-07-31 19:05:29 +0300

    generate web page for first issue

--- bin/generate.pl
+++ bin/generate.pl
@@ -4,16 +4,20 @@ use warnings;
 
 use Data::Dumper qw(Dumper);
 use Template;
-use YAML qw(LoadFile);
 use JSON qw(from_json);
 use File::Slurp qw(read_file);
 
-my $target = shift;
-if (not $target or $target !~ /^(mail|web)$/) {
-	print <<"END_USAGE";
+my ($target, $issue) = @ARGV;
+if (not $target
+	or not $issue
+	or $target !~ /^(mail|web)$/
+	or $issue =~ /\D/) {
+	warn <<"END_USAGE";
 Usage: $0 
-   web
-   mail
+   web   ISSUE 
+   mail  ISSUE
+   
+   ISSUE is a number
 END_USAGE
 	exit;
 }
@@ -21,11 +25,8 @@ END_USAGE
 
 my $t = Template->new();
 
-my $data = from_json scalar read_file 'src/1.json';
-#my $yaml = 'src/1.yml';
-#my $data = LoadFile($yaml);
-#print Dumper $data;
-#print to_json $data, { utf8 => 1, pretty => 1 };
+my $data = from_json scalar read_file "src/$issue.json";
 $data->{$target} = 1;
+$data->{issue} = $issue;
 
 $t->process('page.tt', $data);
