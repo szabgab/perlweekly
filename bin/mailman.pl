120fba19dd958c3e07acd1e76140787f50efdf70
Gabor Szabo <szabgab@gmail.com>
2011-08-05 23:17:08 -0700

    add script to fetch information from mailman

---
+++ bin/mailman.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use WWW::Mailman;
+use Data::Dumper qw(Dumper);
+
+my $pw = shift;
+if (not $pw) {
+   print "Admin pw: ";
+   chomp($pw = <STDIN>);
+}
+
+my $m = WWW::Mailman->new(
+   uri  => 'http://mail.perlweekly.com/mailman/listinfo/perlweekly',
+   admin_password => $pw,
+);
+
+#my @roster = $m->roster;
+#print Dumper $roster[0];
+#print scalar @roster;
+#print Dumper $m->admin_general;
+$m->list_members;
+
