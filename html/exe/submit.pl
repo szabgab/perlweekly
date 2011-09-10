78d21fe39846c85f80315a6088f13b6232811cf0
Gabor Szabo <szabgab@gmail.com>
2011-09-10 21:21:05 +0300

    add simple cgi script to redirect subscription request and send javascript submissio to our own server

---
+++ html/exe/submit.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -T
+use strict;
+use warnings;
+
+use CGI;
+use Email::Valid;
+use LWP::Simple qw(get);
+
+my $q = CGI->new;
+print $q->header;
+my $email = $q->param('email');
+
+if (not $email) {
+	warn "No e-mail given" if not $email;
+} elsif (Email::Valid->address($email)) {
+	get("http://mail.perlweekly.com/mailman/subscribe/perlweekly?email=$email");
+} else {
+	warn "Invalid e-mail '$email'";
+}
+
+print "OK";
+
--- html/js/mailman.js
+++ html/js/mailman.js
@@ -1,9 +1,11 @@
  $(document).ready(function() {
    $("form.subscribe").submit(function() {
-     var url = $(this).attr('action') + '?' + $(this).serialize();
+     //var url = $(this).attr('action') + '?' + $(this).serialize();
+     var url = '/exe/submit.pl' + '?' + $(this).serialize();
      $.get(url, function(r) {
          window.location = "/thankyou.html";
-     });
+     })
+     .error(function() { alert("Sorry. Some error happened. Please let Gabor know.") });
      //$.post(url, function(r) {
      //$.post($(this).attr('action'), $(this).serialize(), function(r) {
      //   alert(r);
