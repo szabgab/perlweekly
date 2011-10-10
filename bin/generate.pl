9e711e52244f1efe014555f625ca3789748659c8
Gabor Szabo <szabgab@gmail.com>
2011-10-10 23:01:56 +0200

    add next template with next date

--- bin/generate.pl
+++ bin/generate.pl
@@ -54,9 +54,10 @@ if ($target eq 'rss') {
         $target = 'rss';
         generate_rss($max);
 
+        my $next = get_data('next');
         my $t = Template->new();
         $t->process('tt/archive.tt', {issues => \@issues}, 'html/archive/index.html') or die $t->error;
-        $t->process('tt/index.tt', {latest => $max, next_issue => 'October 17'}, 'html/index.html') or die $t->error;
+        $t->process('tt/index.tt', { latest => $max, next_issue => $next->{date} }, 'html/index.html') or die $t->error;
         foreach my $f (qw(thankyou unsubscribe)) {
               $t->process("tt/$f.tt", {}, "html/$f.html") or die $t->error;
         }
