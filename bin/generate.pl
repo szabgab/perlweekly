292c64d2009a9020e9e0220e83d419e9f56bf724
Aristotle Pagaltzis <pagaltzis@gmx.de>
2012-03-18 23:44:26 +0100

    split ->generate

--- bin/generate.pl
+++ bin/generate.pl
@@ -94,70 +94,93 @@ sub generate {
     my @out = @_ ? shift : ();
 
     if ($target eq 'mail' or $target eq 'text') {
-        foreach my $ch (@{ $self->{chapters} }) {
-           foreach my $e (@{ $ch->{entries} }) {
-              $e->{url} = $e->{link} || $e->{url};
-           }
-        }
+        $self->fixup_links;
     }
 
     if ($target eq 'text') {
-       foreach my $h (@{ $self->{header} }) {
-         $h = wrap('', '', $h);
-       }
-       foreach my $ch (@{ $self->{chapters} }) {
-          foreach my $e (@{ $ch->{entries} }) {
-              $e->{text} = wrap('', '  ', $e->{text});
-          }
-       }
+        $self->wrap_text;
     }
 
     if ($target ne 'rss') {
-        my $t = Template->new();
-        my $tmpl = $target eq 'text' ? 'tt/text.tt' : 'tt/page.tt';
-        $t->process($tmpl, $self, @out) or die $t->error;
+        return $self->process_tt($target eq 'text' ? 'tt/text.tt' : 'tt/page.tt', @out);
     }
     else {
-        my $url = 'http://perlweekly.com/';
-        my $rss = XML::RSS->new( version => '1.0' );
-        my $year = 1900 + (localtime)[5];
-        $rss->channel(
-            title       => 'Perl Weekly newsletter',
-            link        => $url,
-            description => 'A free, once a week e-mail round-up of hand-picked news and articles about Perl.',
-            dc => {
-                language  => 'en-us',
-                publisher => 'szabgab@gmail.com',
-                rights    => "Copyright 2011-$year, Gabor Szabo",
-            },
-            syn => {
-                updatePeriod     => "weekly",
-                updateFrequency  => "1",
-                updateBase       => "2011-08-01T00:00+00:00",
-            }
-        );
-
-#        $self->{title};
-#        $self->{header};
-        foreach my $ch (@{ $self->{chapters} }) {
-            #$ch->{title}
-            foreach my $e (@{ $ch->{entries} }) {
-                my $text = $e->{text};
-                $rss->add_item(
-                    title => decode('utf-8', $e->{title}),
-                    link  => $e->{url},
-                    description => decode('utf-8', $e->{text}),
-                    #dc => {
-                    #    creator => '???', # TODO should be the author of the original article
-                    #    date    => POSIX::strftime("%Y-%m-%dT%H:%M:%S+00:00", localtime $e->{timestamp},
-                    #    subject => 'list of tags?',
-                );
-            }
-        }
-
-        #rss_item_count();
-        $rss->save( 'html/perlweekly.rss' );
-        return;
-        #return $rss->as_string;
+        $self->process_rss;
     }
+}
+
+sub fixup_links {
+    my $self = shift;
+    foreach my $ch (@{ $self->{chapters} }) {
+       foreach my $e (@{ $ch->{entries} }) {
+          $e->{url} = $e->{link} || $e->{url};
+       }
+    }
+    return $self;
+}
+
+sub wrap_text {
+    my $self = shift;
+    foreach my $h (@{ $self->{header} }) {
+        $h = wrap('', '', $h);
+    }
+    foreach my $ch (@{ $self->{chapters} }) {
+        foreach my $e (@{ $ch->{entries} }) {
+            $e->{text} = wrap('', '  ', $e->{text});
+        }
+    }
+    return $self;
+}
+
+sub process_tt {
+    my $self = shift;
+    my $tmpl = shift;
+    my $t = Template->new();
+    $t->process($tmpl, $self, @_) or die $t->error;
+}
+
+sub process_rss {
+    my $self = shift;
+
+    my $url = 'http://perlweekly.com/';
+    my $rss = XML::RSS->new( version => '1.0' );
+    my $year = 1900 + (localtime)[5];
+    $rss->channel(
+        title       => 'Perl Weekly newsletter',
+        link        => $url,
+        description => 'A free, once a week e-mail round-up of hand-picked news and articles about Perl.',
+        dc => {
+            language  => 'en-us',
+            publisher => 'szabgab@gmail.com',
+            rights    => "Copyright 2011-$year, Gabor Szabo",
+        },
+        syn => {
+            updatePeriod     => "weekly",
+            updateFrequency  => "1",
+            updateBase       => "2011-08-01T00:00+00:00",
+        }
+    );
+
+#    $self->{title};
+#    $self->{header};
+    foreach my $ch (@{ $self->{chapters} }) {
+        #$ch->{title}
+        foreach my $e (@{ $ch->{entries} }) {
+            my $text = $e->{text};
+            $rss->add_item(
+                title => decode('utf-8', $e->{title}),
+                link  => $e->{url},
+                description => decode('utf-8', $e->{text}),
+                #dc => {
+                #    creator => '???', # TODO should be the author of the original article
+                #    date    => POSIX::strftime("%Y-%m-%dT%H:%M:%S+00:00", localtime $e->{timestamp},
+                #    subject => 'list of tags?',
+            );
+        }
+    }
+
+    #rss_item_count();
+    $rss->save( 'html/perlweekly.rss' );
+    return;
+    #return $rss->as_string;
 }
