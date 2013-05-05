6ad72b9c67303ec5212f2ae038b1ea289e7cebfc
Gabor Szabo <szabgab@gmail.com>
2013-05-05 09:07:29 +0300

    gravatar fetching

--- bin/gravatar.pl
+++ bin/gravatar.pl
@@ -12,7 +12,9 @@ die "Usage: $0 email file\n" if not $email or not $file;
 #my $gravatar_id  = gravatar_id($email);
 #say $gravatar_id;
 
-my $gravatar_url = gravatar_url(email => $email, size => 80);
+my $size = 128; # Perl Maven
+#my $size = 80; # Perl weekly
+my $gravatar_url = gravatar_url(email => $email, size => $size);
 die if not $gravatar_url;
 #say $gravatar_url;
 getstore($gravatar_url, $file);
