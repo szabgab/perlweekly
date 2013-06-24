edbdd5f7bb2433facf66547ffe82e61c2cff92a4
Gabor Szabo <szabgab@gmail.com>
2013-06-24 08:38:19 +0300

    improved handling of Unicode characters in the bitly script

--- bin/bitly.pl
+++ bin/bitly.pl
@@ -5,17 +5,21 @@ use JSON         qw(from_json to_json);
 use File::Slurp  qw(read_file write_file);
 use WWW::Shorten::Bitly;
 use Data::Dumper qw(Dumper);
+use Encode       qw(decode encode);
 
 open my $fh, '<', '/home/gabor/.bitly' or die;
 chomp(my $line = <$fh>);
+close $fh;
 my ($user, $apikey) = split /:/, $line;
 
 my $json_file = shift or die "Usage: $0 src/ddd.json\n";
+#binmode(STDOUT, ":utf8");
+#binmode(STDERR, ":utf8");
 
 
-my $data = from_json scalar read_file $json_file, binmode => 'utf8';
-#print Dumper $data;
-#print to_json $data, {utf8 => 1, pretty => 1};
+
+my $src_json = encode('utf-8', scalar read_file($json_file, binmode => ':utf8'));
+my $data = from_json $src_json, { utf8  => 1 };
 
 for my $ch (@{ $data->{chapters} }) {
     for my $e (@{ $ch->{entries} }) {
@@ -29,4 +33,4 @@ for my $ch (@{ $data->{chapters} }) {
 
 
 #write_file $json_file, to_json($data, {utf8 => 1, pretty => 1});
-write_file $json_file, { binmode => 'utf8' }, to_json($data, {utf8 => 1, pretty => 1, canonical => 1});
+write_file $json_file, { binmode => ':utf8' }, decode('utf-8', to_json($data, {utf8 => 1, pretty => 1, canonical => 1}));
