10ea53338d9e8dd400095467a479c6ca0d4e5f42
Gabor Szabo <gabor@szabgab.com>
2013-09-23 08:31:16 +0200

    bitly for embedded urls

--- bin/bitly.pl
+++ bin/bitly.pl
@@ -1,5 +1,7 @@
 use strict;
 use warnings;
+use autodie;
+use 5.010;
 
 use JSON         qw(from_json to_json);
 use File::Slurp  qw(read_file write_file);
@@ -7,7 +9,8 @@ use WWW::Shorten::Bitly;
 use Data::Dumper qw(Dumper);
 use Encode       qw(decode encode);
 
-open my $fh, '<', '/home/gabor/.bitly' or die;
+my $bitly_file = '/home/gabor/.bitly';
+open my $fh, '<', $bitly_file;
 chomp(my $line = <$fh>);
 close $fh;
 my ($user, $apikey) = split /:/, $line;
@@ -23,12 +26,24 @@ my $data = from_json $src_json, { utf8  => 1 };
 
 for my $ch (@{ $data->{chapters} }) {
     for my $e (@{ $ch->{entries} }) {
+        my (@urls) = $e->{text} =~ m{<a href=(https?://[^>]*)>}g;
+        push @urls, $e->{text} =~ m{<a href="(https?://[^>]*)">}g;
+        warn Dumper \@urls;
+        foreach my $url (@urls) {
+            if (not $e->{map}{$url}) {
+                my $bitly = WWW::Shorten::Bitly->new(URL => $url, USER => $user, APIKEY => $apikey);
+                $e->{map}{$url} = $bitly->shorten(URL => $url);
+            }
+        }
+        warn Dumper $e->{map};
+
         next if $e->{link};
 		next if not $e->{url};
-        print "$e->{url}\n";
+        say $e->{url};
         #$e->{url} .= "&utm_medium=email&utm_campaign=PerlWeekly_$self->{issue}";
         my $bitly = WWW::Shorten::Bitly->new(URL => $e->{url}, USER => $user, APIKEY => $apikey);
         $e->{link} = $bitly->shorten(URL => $e->{url});
+        say $e->{link};
     }
 }
 
