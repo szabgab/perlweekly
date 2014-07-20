892d22e1fcfbd34d734f6dcadda245bc6b470278
Yanick Champoux <yanick@babyl.dyndns.org>
2014-07-20 12:38:27 -0400

    wip

---
+++ bin/discover_from_week.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl 
+
+use 5.12.0;
+
+use strict;
+use warnings;
+
+use JSON::Path;
+use Path::Tiny qw/ path /;
+use URI;
+use XML::Feed;
+
+my( $feeds_file, $week_file ) = map { path($_) } @ARGV;
+
+my $json = $week_file->slurp;
+
+my $jpath = JSON::Path->new( '$.chapters[*].entries[*].url' );
+
+my @urls = map { URI->new($_) } $jpath->values($json);
+
+my %already_seen;
+
+for ( $feeds_file->read_lines ) {
+    chomp;
+    s/#.*//;
+    next if /^\s*$/;
+    $already_seen{ URI->new($_)->host }++;
+}
+
+for my $url ( @urls ) {
+    next if $already_seen( $url->host );
+
+    for my $feed ( XML::Feed->find_feeds($url) ) {
+        warn "adding $feed\n";
+        $feeds_file->append( "$feed\n" );
+    }
+
+    $already_seen{$url->host}++;
+}
