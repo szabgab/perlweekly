c0c884b31a87e0ef8fd21106c94eaece9a2da7ec
Gabor Szabo <gabor@szabgab.com>
2014-10-08 16:04:01 +0200

    gravata size

--- bin/gravatar.pl
+++ bin/gravatar.pl
@@ -12,9 +12,9 @@ die "Usage: $0 email file\n" if not $email or not $file;
 #my $gravatar_id  = gravatar_id($email);
 #say $gravatar_id;
 
-my $size = 128;    # Perl Maven
+#my $size = 128;    # Perl Maven
 
-#my $size = 80; # Perl weekly
+my $size = 80; # Perl weekly
 my $gravatar_url = gravatar_url( email => $email, size => $size );
 die if not $gravatar_url;
 
