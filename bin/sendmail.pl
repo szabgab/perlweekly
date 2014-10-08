5250ae370ef8abd9bf9ecb680a734e6a0392164c
Gabor Szabo <gabor@szabgab.com>
2014-10-08 16:35:21 +0200

    send e-mail From: the current editor

--- bin/sendmail.pl
+++ bin/sendmail.pl
@@ -5,6 +5,7 @@ use warnings;
 use Getopt::Long qw(GetOptions);
 use MIME::Lite;
 use Cwd qw(abs_path cwd);
+use Data::Dumper qw(Dumper);
 use Path::Tiny qw(path);
 use JSON qw(from_json);
 use Encode qw(decode encode);
@@ -14,10 +15,6 @@ GetOptions( \%opt, 'to=s', 'issue=i', 'smtp=s' ) or die;
 die "Usage: $0 --to mail\@address.com  --issue N [--smtp server]\n"
 	if not $opt{to}
 	or not $opt{issue};
-
-my $from = 'Gabor Szabo <gabor@szabgab.com>';
-
-#my $from = 'Yanick Champoux <yanick@babyl.ca>';
 
 my $subject = 'The current Perl Weekly News - Issue #' . $opt{issue};
 
@@ -29,6 +26,12 @@ my $data = from_json scalar( path("src/$opt{issue}.json")->slurp_utf8 );
 if ( $data->{subject} ) {
 	$subject = "#$opt{issue} - $data->{subject}";
 }
+
+my $editors = from_json scalar( path("src/editors.json")->slurp_utf8 );
+#die $data->{editor};
+#die Dumper $editors;
+
+my $from = "$editors->{ $data->{editor} }{name} <$editors->{ $data->{editor} }{from}>";
 
 my $msg = MIME::Lite->new(
 	From    => $from,
