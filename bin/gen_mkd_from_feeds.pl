f14c6430f7e66c4a3c581b77e6ff876a1c437511
Gabor Szabo <gabor@szabgab.com>
2015-09-07 08:05:46 +0200

    tidy

--- bin/gen_mkd_from_feeds.pl
+++ bin/gen_mkd_from_feeds.pl
@@ -11,7 +11,8 @@ use URI;
 use DateTime::Functions qw/ now /;
 use Path::Tiny;
 
-my @feeds = map { s/\s.*$//r; } grep { /^[^#]/ and not /^\s*$/ } path(shift)->lines( { chomp => 1 });
+my @feeds = map { s/\s.*$//r; }
+	grep { /^[^#]/ and not /^\s*$/ } path(shift)->lines( { chomp => 1 } );
 
 my $cutout_date = now();
 $cutout_date->subtract( days => 1 ) until $cutout_date->day_of_week == 1;
@@ -20,7 +21,7 @@ my $target = path(shift);
 
 my %seen;
 
-%seen = map { $_ => 1 } grep { /^http/ } $target->lines( { chomp => 1 });
+%seen = map { $_ => 1 } grep {/^http/} $target->lines( { chomp => 1 } );
 
 my @entries = sort { $a->issued <=> $b->issued }
 	grep { not $seen{ $_->link }++ }
@@ -29,11 +30,9 @@ my @entries = sort { $a->issued <=> $b->issued }
 	map  { XML::Feed->parse( URI->new($_) ) } @feeds;
 
 for my $entry (@entries) {
-    $target->append(
-        join "\n", 
-	'### '. $entry->title,
-	$entry->link,
-	eval { $entry->issued->ymd } || '????-??-??',
-	$entry->author, "\n\n",
-    )
+	$target->append(
+		join "\n", '### ' . $entry->title,
+		$entry->link, eval { $entry->issued->ymd } || '????-??-??',
+		$entry->author, "\n\n",
+	);
 }
