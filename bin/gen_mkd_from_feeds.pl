f6cc18ab2ddbf5d477a2f0aff3b1c8e4b6f4a65b
Yanick Champoux <yanick@babyl.dyndns.org>
2015-10-25 19:13:07 -0400

    almost done

--- bin/gen_mkd_from_feeds.pl
+++ bin/gen_mkd_from_feeds.pl
@@ -10,6 +10,9 @@ use URI;
 
 use DateTime::Functions qw/ now /;
 use Path::Tiny;
+use File::Serialize;
+
+use experimental 'postderef', 'smartmatch';
 
 my @feeds = map { s/\s.*$//r; }
 	grep { /^[^#]/ and not /^\s*$/ } path(shift)->lines( { chomp => 1 } );
@@ -19,11 +22,19 @@ $cutout_date->subtract( days => 1 ) until $cutout_date->day_of_week == 1;
 
 my $target = path(shift);
 
+my @in_archive;
+# read all the past entries and collect the urls
+for my $archive ( grep { $_->basename =~ /^\d+\.json/ } path('src')->children ) {
+    my $pw = deserialize_file $archive;
+    push @in_archive, map { $_->{url} } map { $_->{entries}->@* } $pw->{chapters}->@*;
+}
+
 my %seen;
 
 %seen = map { $_ => 1 } grep {/^http/} $target->lines( { chomp => 1 } );
 
 my @entries = sort { $a->issued <=> $b->issued }
+    grep { !( $_->link ~~ @in_archive ) or do { warn $_->link, " seen in archive"; 0 } }
 	grep { not $seen{ $_->link }++ }
 	grep { $_->issued >= $cutout_date }
 	map  { $_->entries }
