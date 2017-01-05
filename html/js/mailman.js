8bde6568c4cb338e86cfe8254586646e2391de4c
Gabor Szabo <gabor@szabgab.com>
2017-01-05 11:20:52 +0200

    skip the exe on the server

--- html/js/mailman.js
+++ html/js/mailman.js
@@ -1,16 +1,18 @@
- $(document).ready(function() {
-   $("form.subscribe").submit(function() {
-     //var url = $(this).attr('action') + '?' + $(this).serialize();
-     var url = '/exe/submit.pl' + '?' + $(this).serialize();
-     $.get(url, function(r) {
-         window.location = "/thankyou.html";
-     })
-     .error(function() { alert("Sorry. Some error happened. Please let Gabor know.") });
-     //$.post(url, function(r) {
-     //$.post($(this).attr('action'), $(this).serialize(), function(r) {
-     //   alert(r);
-     //});
-     return false;
-   });
- });
+// Stopped using it as the command-line script that submits the subscription request
+// does NOT work on https.
+// $(document).ready(function() {
+//   $("form.subscribe").submit(function() {
+//     //var url = $(this).attr('action') + '?' + $(this).serialize();
+//     var url = '/exe/submit.pl' + '?' + $(this).serialize();
+//     $.get(url, function(r) {
+//         window.location = "/thankyou.html";
+//     })
+//     .error(function() { alert("Sorry. Some error happened. Please let Gabor know.") });
+//     //$.post(url, function(r) {
+//     //$.post($(this).attr('action'), $(this).serialize(), function(r) {
+//     //   alert(r);
+//     //});
+//     return false;
+//   });
+// });
 
