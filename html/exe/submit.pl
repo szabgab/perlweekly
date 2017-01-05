052b8303bace181cdb05f005bf5d0e3099f72bb0
Gabor Szabo <gabor@szabgab.com>
2017-01-05 11:05:31 +0200

    https in subscribe request

--- html/exe/submit.pl
+++ html/exe/submit.pl
@@ -13,7 +13,7 @@ my $email = $q->param('email');
 if (not $email) {
 	warn "No e-mail given";
 } elsif (Email::Valid->address($email)) {
-	get("http://mail.perlweekly.com/mailman/subscribe/perlweekly?email=$email");
+	get("https://mail.perlweekly.com/mailman/subscribe/perlweekly?email=$email");
 } else {
 	warn "Invalid e-mail '$email'";
 }
