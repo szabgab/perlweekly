4f2b73d4ecd805b8ea7581dbd9aa09738d6907de
Gabor Szabo <gabor@szabgab.com>
2018-06-25 08:38:15 +0300

    tidy code

--- bin/tidy_json.pl
+++ bin/tidy_json.pl
@@ -9,27 +9,26 @@ use Encode qw/ encode decode /;
 
 main();
 
-
 sub main {
-    foreach my $file ( glob "src/*.json" ) {
-        next if $file !~ m{/\d+.json$};
-        tidy($file);
-    }
-    foreach my $file ( "src/authors.json", "src/stickeryou.json" ) {
-        tidy($file);
-    }
+	foreach my $file ( glob "src/*.json" ) {
+		next if $file !~ m{/\d+.json$};
+		tidy($file);
+	}
+	foreach my $file ( "src/authors.json", "src/stickeryou.json" ) {
+		tidy($file);
+	}
 }
 
 sub tidy {
-    my ($file) = @_;
-    say $file;
+	my ($file) = @_;
+	say $file;
 
-    my $src_json = encode( 'utf-8', scalar path($file)->slurp_utf8 );
-    my $data = from_json( $src_json, { utf8 => 1 } );
-    path($file)->spew_utf8(
-        decode(
-            'utf-8',
-            to_json( $data, { utf8 => 1, pretty => 1, canonical => 1 } )
-        )
-    );
+	my $src_json = encode( 'utf-8', scalar path($file)->slurp_utf8 );
+	my $data = from_json( $src_json, { utf8 => 1 } );
+	path($file)->spew_utf8(
+		decode(
+			'utf-8',
+			to_json( $data, { utf8 => 1, pretty => 1, canonical => 1 } )
+		)
+	);
 }
