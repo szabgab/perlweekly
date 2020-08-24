e8a49780fc9414e9caa9a1951cd60589ce683a56
Gabor Szabo <gabor@szabgab.com>
2020-08-24 08:43:59 +0300

    collect link to repo and link to CI systems as well

--- bin/metacpan.pl
+++ bin/metacpan.pl
@@ -4,7 +4,19 @@ use 5.010;
 use MetaCPAN::Client ();
 use DateTime         ();
 
+use File::Spec ();
+use Log::Log4perl ();
+use Log::Log4perl::Level ();
+use FindBin ();
+
+# clone https://github.com/szabgab/cpan-digger-new
+use lib File::Spec->catdir($FindBin::Bin, '..', '..', 'cpan-digger-new');
+use CPANDigger qw(get_data);
+
 my $recent = 1000;
+
+my $log_level = 'DEBUG';
+Log::Log4perl->easy_init(Log::Log4perl::Level::to_priority( $log_level ));
 
 my $dt = DateTime->now;
 die "We are only supposed to run this on Monday!\n" if $dt->day_of_week != 1;
@@ -17,14 +29,28 @@ my $total = 0;
 my %distros;
 my %authors;
 my $done = 0;
+my $vcs_count = 0;
+my $ci_count = 0;
 while ( my $item = $rset->next ) {
-	if ( $last_monday le $item->date and $item->date lt $today ) {
-		$total++;
-		$distros{ $item->distribution }++;
-		say $item->author;
-		$authors{ $item->author }++;
-	}
 	$done = $item->date le $last_monday;
+
+	next if $item->date lt $last_monday;
+    next if $today le $item->date;
+
+	$total++;
+    my $already_seen = $distros{ $item->distribution };
+	$distros{ $item->distribution }++;
+	say $item->author;
+	$authors{ $item->author }++;
+    next if $already_seen;
+
+    my %data = get_data($item);
+    if ($data{vcs_url}) {
+        $vcs_count++;
+    }
+    if ($data{travis} or $data{github_actions}) {
+        $ci_count++;
+    }
 }
 
 if ( not $done ) {
@@ -33,6 +59,6 @@ if ( not $done ) {
 }
 
 printf
-	"Last week there were a total of %s uploads to CPAN of %s distinct distributions by %s different authors.\n",
-	$total, scalar( keys %distros ), scalar( keys %authors ),
+	"Last week there were a total of %s uploads to CPAN of %s distinct distributions by %s different authors. Number of distributions with link to VCS: %s. Number of distros with CI: %s.\n",
+	$total, scalar( keys %distros ), scalar( keys %authors ), $vcs_count, $ci_count;
 
