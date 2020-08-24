0d271d0864fd2433809ce7f5095c241495de6f79
Gabor Szabo <gabor@szabgab.com>
2020-08-24 08:44:26 +0300

    tidy

--- bin/metacpan.pl
+++ bin/metacpan.pl
@@ -4,19 +4,19 @@ use 5.010;
 use MetaCPAN::Client ();
 use DateTime         ();
 
-use File::Spec ();
-use Log::Log4perl ();
+use File::Spec           ();
+use Log::Log4perl        ();
 use Log::Log4perl::Level ();
-use FindBin ();
+use FindBin              ();
 
 # clone https://github.com/szabgab/cpan-digger-new
-use lib File::Spec->catdir($FindBin::Bin, '..', '..', 'cpan-digger-new');
+use lib File::Spec->catdir( $FindBin::Bin, '..', '..', 'cpan-digger-new' );
 use CPANDigger qw(get_data);
 
 my $recent = 1000;
 
 my $log_level = 'DEBUG';
-Log::Log4perl->easy_init(Log::Log4perl::Level::to_priority( $log_level ));
+Log::Log4perl->easy_init( Log::Log4perl::Level::to_priority($log_level) );
 
 my $dt = DateTime->now;
 die "We are only supposed to run this on Monday!\n" if $dt->day_of_week != 1;
@@ -28,29 +28,30 @@ my $rset  = $mcpan->recent($recent);
 my $total = 0;
 my %distros;
 my %authors;
-my $done = 0;
+my $done      = 0;
 my $vcs_count = 0;
-my $ci_count = 0;
+my $ci_count  = 0;
+
 while ( my $item = $rset->next ) {
 	$done = $item->date le $last_monday;
 
 	next if $item->date lt $last_monday;
-    next if $today le $item->date;
+	next if $today le $item->date;
 
 	$total++;
-    my $already_seen = $distros{ $item->distribution };
+	my $already_seen = $distros{ $item->distribution };
 	$distros{ $item->distribution }++;
 	say $item->author;
 	$authors{ $item->author }++;
-    next if $already_seen;
+	next if $already_seen;
 
-    my %data = get_data($item);
-    if ($data{vcs_url}) {
-        $vcs_count++;
-    }
-    if ($data{travis} or $data{github_actions}) {
-        $ci_count++;
-    }
+	my %data = get_data($item);
+	if ( $data{vcs_url} ) {
+		$vcs_count++;
+	}
+	if ( $data{travis} or $data{github_actions} ) {
+		$ci_count++;
+	}
 }
 
 if ( not $done ) {
@@ -60,5 +61,6 @@ if ( not $done ) {
 
 printf
 	"Last week there were a total of %s uploads to CPAN of %s distinct distributions by %s different authors. Number of distributions with link to VCS: %s. Number of distros with CI: %s.\n",
-	$total, scalar( keys %distros ), scalar( keys %authors ), $vcs_count, $ci_count;
+	$total, scalar( keys %distros ), scalar( keys %authors ), $vcs_count,
+	$ci_count;
 
