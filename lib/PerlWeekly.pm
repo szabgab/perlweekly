d50a52de79d9eb4b0dfcf0853576a31560791c00
Gabor Szabo <gabor@szabgab.com>
2023-01-16 08:10:29 +0200

    check if the 'name' field isn't missing in an author entry

--- lib/PerlWeekly.pm
+++ lib/PerlWeekly.pm
@@ -2,6 +2,7 @@ package PerlWeekly;
 use strict;
 use warnings;
 
+use Data::Dumper qw(Dumper);
 use Exporter qw(import);
 use JSON qw(from_json);
 use Path::Tiny qw(path);
@@ -13,6 +14,7 @@ sub get_authors {
 		= eval { from_json scalar( path("src/authors.json")->slurp_utf8 ) };
 	die "Could not read src/authors.json\n\n$@" if $@;
 	foreach my $author ( keys %$authors ) {
+        die "Name missing from author '$author' in the src/authors.json file\n" if not exists $authors->{$author}{name};
 		$authors->{$author}{key} = $author;
 		( $authors->{$author}{handler} = $author ) =~ s/_/-/g;
 	}
